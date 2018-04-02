5578d91229c8c35e254994ac5eb207ecf0785011
D7703_15 <d7703_15@D7703-15ui-iMac.local>
2018-04-02 15:49:49 +0900

    imageView

--- Aliean imageView/ViewController.swift
+++ Aliean imageView/ViewController.swift
@@ -9,17 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var count = 1 //사진 인엑스 값 증가
+    var direction = 1 //direction이 1이면 증가 0이면 감소 구분
+    @IBOutlet weak var myimageview: UIImageView!
+    @IBOutlet weak var index: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // 첫번째 이미지를 로드함
+        myimageview.image = UIImage(named: "frame1.png")
+        index.text = "1"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func imageUpdata(_ sender: Any) {
+        if count == 5 {
+            direction = 0
+        }
+        else if count == 1 {
+            direction = 1
+        }
+        if direction == 1 {
+            count += 1 }
+        else if direction == 0 {
+            count = count - 1
+        }
+        myimageview.image = UIImage(named: "frame\(count).png")
+        index.text = String(count)
     }
-
-
 }
+        
+  
 
